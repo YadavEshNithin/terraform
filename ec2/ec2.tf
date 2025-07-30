acc26e4f0763402d8dc0d8d399b16c551725168a
YadavEshnithin <eshnithinyadav@gmail.com>
2025-07-30 16:01:20 +0530

    3.86.52.173

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "HelloWorld_terraform" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t2.micro"
   vpc_security_group_ids = local.sg_id
+  
 
   tags = {
     Name = "HelloWorld"
