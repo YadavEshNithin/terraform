5631f3c42acd220dc671dd5ec7417724c45ecaa7
YadavEshnithin <eshnithinyadav@gmail.com>
2025-09-08 13:13:36 +0530

    update

--- docker_instance_creation/docker.sh
+++ docker_instance_creation/docker.sh
@@ -32,4 +32,52 @@ git clone https://github.com/YadavEshNithin/terraform.git
 
 cd terraform/docker_instance_creation/
 
-eksctl create cluster --config-file=eks.yaml
+# eksctl create cluster --config-file=eks.yaml
+
+
+##########################
+
+#!/bin/bash
+
+growpart /dev/xvda 4
+
+lvextend -L +10G /dev/mapper/RootVG-homeVol
+
+lvextend -L +10G /dev/mapper/RootVG-varVol
+
+lvextend -l +100%FREE /dev/mapper/RootVG-varTmpVol
+
+xfs_growfs /home
+
+xfs_growfs /var
+
+xfs_growfs /var/tmp
+
+
+
+#############
+
+
+#!/bin/bash
+
+growpart /dev/xvda 4
+
+lvextend -L +10G /dev/RootVG/rootVol
+lvextend -L +10G /dev/RootVG/varVol
+lvextend -l +100%FREE /dev/RootVG/varTmpVol
+
+xfs_growfs /
+
+xfs_growfs /var
+
+xfs_growfs /var/tmp
+
+
+curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+# Add required dependencies for the jenkins package
+yum install fontconfig java-21-openjdk -y
+yum install jenkins -y
+systemctl daemon-reload
+systemctl enable jenkins
+systemctl start jenkins
